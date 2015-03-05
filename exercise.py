a24c78c3ba2fca69de8394c3a77589bccadcc8f0
gisphm <phmfk@hotmail.com>
2015-03-05 18:51:12 +0800

    读取并写入文件

--- exercise.py
+++ exercise.py
@@ -5,14 +5,29 @@ from sys import argv
 
 script, file_name = argv
 
-txt = open(file_name)
+print "We're going to erase %r." % file_name
+print "If you don't want that, hit CTRL-C (^C)."
+print "If you do want that, hit RETURN."
 
-print "Here's your file %r:" % file_name
-print txt.read()
+raw_input("?")
 
-print "Type the filename again:"
-file_again = raw_input("> ")
+print "Opening the file ..."
+target = open(file_name, 'w')
 
-txt_again = open(file_again)
+print "Now I'm going to ask you for three lines."
 
-print txt_again.read()
+line1 = raw_input("line 1: ")
+line2 = raw_input("line 2: ")
+line3 = raw_input("line 3: ")
+
+print "I'm going to write these to the file."
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.write("\n")
+
+print "And finally, we close it."
+target.close()
