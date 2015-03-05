2d112b811c440aeb0c282d34c521371c28349684
gisphm <phmfk@hotmail.com>
2015-03-05 16:27:16 +0800

    更多的输入

--- exercise.py
+++ exercise.py
@@ -1,22 +1,22 @@
 # -*- coding=utf-8 -*-
 
-x = "There are %d types of people." % 10
-binary = "binary"
-do_not = "don't"
-y = "Those who know %s and those who %s." % (binary, do_not)
+print "Mary had a little lamb."
+print "It's fleece was white as %s." % 'snow'
+print "And everywhere that Mary went."
+print "." * 10
 
-print x
-print y
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
 
-print "I said: %r." % x
-print "I also said: '%s'." % y
-
-hilarious = False
-joke_evaluation = "Isn't that joke so funny?! %r"
-
-print joke_evaluation % hilarious
-
-w = "This is the left side of ..."
-e = "a string with right side."
-
-print w + e
+print end1 + end2 + end3 + end4 + end5 + end6
+print end7 + end8 + end9 + end10 + end11 + end12
