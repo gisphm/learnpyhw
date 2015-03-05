8fdbaf7efd3ed3a57ac48205b037b268d5ac5e23
gisphm <phmfk@hotmail.com>
2015-03-05 18:15:18 +0800

    命令参数输出

--- exercise.py
+++ exercise.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
-age = raw_input("How old are you? ")
-height = raw_input("How tall are you? ")
-weight = raw_input("How much do you weight? ")
+from sys import argv
 
-print "So, you're %r old, %r tall and %r heavy." % (
-    age, height, weight
-)
+
+script, first, second, third = argv
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
