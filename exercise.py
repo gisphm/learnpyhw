7112a0a5bb754895db5b97e5d0d417458106e5cc
gisphm <phmfk@hotmail.com>
2015-03-05 16:40:07 +0800

    格式化输出，raw输出

--- exercise.py
+++ exercise.py
@@ -1,22 +1,16 @@
-# -*- coding=utf-8 -*-
+# -*- coding: utf-8 -*-
 
-print "Mary had a little lamb."
-print "It's fleece was white as %s." % 'snow'
-print "And everywhere that Mary went."
-print "." * 10
+formatter = "%r %r %r %r"
 
-end1 = "C"
-end2 = "h"
-end3 = "e"
-end4 = "e"
-end5 = "s"
-end6 = "e"
-end7 = "B"
-end8 = "u"
-end9 = "r"
-end10 = "g"
-end11 = "e"
-end12 = "r"
-
-print end1 + end2 + end3 + end4 + end5 + end6
-print end7 + end8 + end9 + end10 + end11 + end12
+print formatter % (1, 2, 3, 4)
+print formatter % ("one", "two", "three", "four")
+print formatter % (True, False, False, True)
+print formatter % (formatter, formatter, formatter, formatter)
+print formatter % (
+    "I had this thing.",
+    "That you could type up right.",
+    "But it didn't sing.",
+    "So I said goodnight."
+)
+print formatter % ('大水', '英语', '理解', '代码')
+print "%s %s %s %s" % ('大水', '英语', '理解', '代码')
