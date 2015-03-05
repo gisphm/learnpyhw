d77e73e4dbe01e2322105515ecc335e820dc1e0e
gisphm <phmfk@hotmail.com>
2015-03-05 17:58:14 +0800

    简单问答，输入

--- exercise.py
+++ exercise.py
@@ -1,17 +1,12 @@
 # -*- coding: utf-8 -*-
 
-tabby_cat = "\tI'm tabbed in."
-persian_cat = "I'm split\non a line."
-backslash_cat = "I'm \\ a \\ cat."
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
 
-fat_cat = '''
-I'll do a list:
-\t* Cat food
-\t* Fishes
-\t* Catnip\n\t* Grass
-'''
-
-print tabby_cat
-print persian_cat
-print backslash_cat
-print fat_cat
+print "So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight
+)
