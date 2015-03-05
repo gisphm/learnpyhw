314f53680a9d767b2a11e2d3b719369deb40d917
gisphm <phmfk@hotmail.com>
2015-03-05 19:54:10 +0800

    函数与参数，形参的各种变化

--- exercise.py
+++ exercise.py
@@ -1,30 +1,24 @@
 # -*- coding: utf-8 -*-
 
 
-def print_two(*args):
-    arg1, arg2 = args
-    print "arg1: %r, arg2: %r" % (arg1, arg2)
+def cheese_and_crackers(cheese_count, boxes_of_crackers):
+    print "You have %d cheeses!" % cheese_count
+    print "You have %d boxes of crackers!" % boxes_of_crackers
+    print "Man that's enough for a party!"
+    print "Get a blanket.\n"
 
 
-def print_two_again(arg1, arg2):
-    print "arg1: %r, arg2: %r" % (arg1, arg2)
+print "We can just give the function numbers directly:"
+cheese_and_crackers(20, 30)
 
+print "OR, we can use variables from our script:"
+amount_of_cheese = 10
+amount_of_crackers = 50
 
-def print_one(arg1):
-    print "arg1: %r" % arg1
+cheese_and_crackers(amount_of_cheese, amount_of_crackers)
 
+print "We can even do math inside too:"
+cheese_and_crackers(10 + 20, 5 + 6)
 
-def print_none():
-    print "I got nothing."
-
-
-def print_something(something):
-    print "Something I want or I don't is my decision."
-    print "It depends %r." % something
-
-
-print_two("Zed", "Shaw")
-print_two_again("Zed", "Shaw")
-print_one("First!")
-print_none()
-print_something("shit")
+print "And we can combine the two, variable and math:"
+cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
